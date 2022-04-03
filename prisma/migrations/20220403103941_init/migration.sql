160b9236e228eb09f7c575c74d75e3a9c1dd3bbb
ghost-in-the-machine-86 <mbdsj4@hotmail.com>
2022-04-03 07:49:54 -0300

    criar app nestjs + add prisma e client

---
+++ prisma/migrations/20220403103941_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL
+);
