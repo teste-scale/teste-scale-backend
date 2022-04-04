faae28ffb09f7a368618ab42950fe4e33e7754a0
ghost-in-the-machine-86 <mbdsj4@hotmail.com>
2022-04-04 10:46:53 -0300

    ajustes na api externa, sort e testes

--- src/utils/helpers/sort.helper.ts
+++ src/utils/helpers/sort.helper.ts
@@ -3,25 +3,35 @@ export class SortHelper {
     arr: any[],
     sortFunction: (left: any, right: any) => boolean,
   ): any[] {
-    const sort = (array: any[]): any[] => {
-      if (array.length < 2) return array;
-      const divider = Math.floor(array.length / 2);
-      const left = array.slice(0, divider);
-      const right = array.slice(divider, array.length);
-      return merge(sort(left), sort(right));
-    };
+    return this.sort(arr, sortFunction);
+  }
 
-    const merge = (left: any[], right: any[]): any[] => {
-      const mergeArray = [];
-      while (left.length && right.length) {
-        sortFunction(left[0], right[0])
-          ? mergeArray.push(left.shift())
-          : mergeArray.push(right.shift());
-      }
-      return mergeArray.concat(left, right);
-    };
+  private static sort(
+    array: any[],
+    sortFunction: (left: any, right: any) => boolean,
+  ): any[] {
+    if (array.length < 2) return array;
+    const divider = Math.floor(array.length / 2);
+    const left = array.slice(0, divider);
+    const right = array.slice(divider, array.length);
+    return this.merge(
+      this.sort(left, sortFunction),
+      this.sort(right, sortFunction),
+      sortFunction,
+    );
+  }
 
-    const sortList = sort(arr);
-    return sortList;
+  private static merge(
+    left: any[],
+    right: any[],
+    sortFunction: (left: any, right: any) => boolean,
+  ): any[] {
+    const mergeArray = [];
+    while (left.length && right.length) {
+      sortFunction(left[0], right[0])
+        ? mergeArray.push(left.shift())
+        : mergeArray.push(right.shift());
+    }
+    return mergeArray.concat(left, right);
   }
 }
